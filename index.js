f54864fb436f20253458f5f220a201632ae9040b
Salaxer <hector_salazar12@hotmail.com>
2021-05-12 15:09:45 -0500

    routes

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ const express = require("express");
 const app = express();
 const { config } = require("./config/index");
 
-app.get("/", (req, res) => {
-  res.send("hello World");
-});
+const moviesApi = require("./routes/movies");
 
-app.get("/date/:year", (req, res) => {
-  let year = req.params.year;
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        ${
-          (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
-            ? "Año bisiesto"
-            : "No es año bisiesto"
-        }
-    </body>
-    </html>
-    `);
-});
+moviesApi(app);
+
+// app.get("/date/:year", (req, res) => {
+//   let year = req.params.year;
+//   res.send(`
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title>Document</title>
+//     </head>
+//     <body>
+//         ${
+//           (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+//             ? "Año bisiesto"
+//             : "No es año bisiesto"
+//         }
+//     </body>
+//     </html>
+//     `);
+// });
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
