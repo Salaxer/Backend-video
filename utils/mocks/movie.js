4a3a55fdb3bc4e7f39b5fa5e22e44a86fcfdc700
Salaxer <hector_salazar12@hotmail.com>
2021-05-17 14:17:24 -0500

    Test are finished because i already done :)

--- utils/mocks/movie.js
+++ utils/mocks/movie.js
@@ -153,6 +153,21 @@ const moviesMock = [
   },
 ];
 
+function filteredMoviesMocks(tag) {
+  return moviesMock.filter((movie) => movie.tags.includes(tag));
+}
+
+class MoviesServiesMock {
+  async getMovie() {
+    return Promise.resolve(moviesMock);
+  }
+  async createMovie() {
+    return Promise.resolve(moviesMock[0]);
+  }
+}
+
 module.exports = {
   moviesMock,
+  filteredMoviesMocks,
+  MoviesServiesMock,
 };
